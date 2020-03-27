18f30d62e077b50955f891d71b559b419ab2c085
Oleksii Aleshchenko <oleksii.aleshchenko@netcracker.com>
2020-03-27 16:19:27 +0200

    fix lab3

--- src/Lab3.java
+++ src/Lab3.java
@@ -1,12 +1,40 @@
 public class Lab3 {
     public static void main(String[] args) {
+        String s = "";
+        String s2 = "";
+//        String s = new String("");
+//        String s2 = new String("");
+        System.out.println(s == s2);
+        for (int i = 0; i < 10; i++) {
+            s += i;
+        }
+        System.out.println(s);
+
+
+        StringBuilder stringBuilder = new StringBuilder(42);
+        for (int i = 0; i < 20; i++) {
+            stringBuilder.append(i % 10);
+        }
+        System.out.println(stringBuilder);
+
+
+
+        /*System.out.println("AAAAAA!");
         String string1 = "ASD";
-        String string2 = string1;
-        string1 = string1 + "C";
+        String string2 = "ASD";
+//        string1 = string1 + "C";
         System.out.println(string1);
         System.out.println(string1 == string2);
+
+        Object object1 = new Object();
+        Object object2 = new Object();
+        Object o = "object1 == object2: " + object1;
+        System.out.println("object1 == object2: " + (object1 == object2));
+
         System.out.println(string1.equals(string2));
         int a = 1;
-        char c = 'a';
+        char c = 'a';*/
+        /*String str = "Умудренный жизненным опытом экспедитор оказался прав! A ты очень веселый. Привет, это веселая игра?";
+        System.out.println(str.replaceAll("([^.!?, ]+)([^.!?]+?)([^.!?, ]+)([.!?])", "$3$2$1$4"));*/
     }
 }
